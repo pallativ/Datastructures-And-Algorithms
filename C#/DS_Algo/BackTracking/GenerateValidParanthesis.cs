b7b22baeed3841f1bd19ebec71d63231bab5168a
Veera Pallati <pkondalu@gmail.com>
2021-07-25 06:43:57 +0530

    Adding Solutions to problems

---
+++ C#/DS_Algo/BackTracking/GenerateValidParanthesis.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Datastructures.BackTracking
+{
+    public class GenerateValidParanthesis
+    {
+        private int max = 0;
+        private List<string> validParathesisList = new List<string>();
+
+        private readonly ITestOutputHelper output;
+
+        public GenerateValidParanthesis(ITestOutputHelper output)
+        {
+            this.output = output;
+        }
+
+
+        public List<string> Generate(int n)
+        {
+            max = n;
+            StringBuilder current = new StringBuilder();
+            BackTrack(current, 0, 0);
+            return validParathesisList;
+        }
+
+        private void BackTrack(StringBuilder currentParanthesis, int open, int close)
+        {
+            
+            if (currentParanthesis.Length == max * 2)
+            {
+                validParathesisList.Add(currentParanthesis.ToString());
+                output.WriteLine("*****");
+                return;
+            }
+           
+            if (open < max)
+            {
+                currentParanthesis.Append("(");
+                output.WriteLine($"({open}, {close}) => {currentParanthesis.ToString()}");
+                BackTrack(currentParanthesis, open + 1, close);
+                currentParanthesis.Remove(currentParanthesis.Length - 1, 1);
+            }
+
+            if (close < open)
+            {
+                currentParanthesis.Append(")");
+                output.WriteLine($"({open}, {close}) => {currentParanthesis.ToString()}");
+                BackTrack(currentParanthesis, open, close + 1);
+                currentParanthesis.Remove(currentParanthesis.Length - 1, 1);
+            }
+        }
+
+        [Theory]
+        [ClassData(typeof(GenerateData))]
+        public void Test(Input input)
+        {
+            var actual = Generate(input.Max);
+            input.Result.Sort();
+            actual.Sort();
+            Assert.Equal(input.Result, actual);
+        }
+    }
+    public class Input
+    {
+        public int Max { get; set; }
+        public List<string> Result { get; set; }
+    }
+    public class GenerateData : IEnumerable<Object[]>
+    {
+        public IEnumerator<object[]> GetEnumerator()
+        {
+            yield return new object[]
+            {
+                new Input()
+                {
+                    Max = 3,
+                    Result = new List<string>(){ "((()))", "(()())", "(())()", "()(())", "()()()" }
+                }
+            };
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+    }
+
+}
---
+++ C#/DS_Algo/DP/0_By_1_KnapsackProblem.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Datastructures.DP
+{
+    public class _0_By_1_KnapsackProblem
+    {
+        public int SolveKnapsack(int[] profits, int[] weights, int capacity)
+        {
+            return SolveKnapsack(profits, weights, capacity, 0);
+        }
+        private int SolveKnapsack(int[] profits, int[] weights, int capacity, int currentIndex)
+        {
+            if (currentIndex >= weights.Length)
+                return 0;
+            if (capacity <= 0) return 0;
+
+            int profitInclusive = 0, profitExclusive = 0;
+            if (capacity >= weights[currentIndex])
+                profitInclusive = profits[currentIndex] + SolveKnapsack(profits, weights, capacity - weights[currentIndex], currentIndex + 1);
+
+            profitExclusive = SolveKnapsack(profits, weights, capacity, currentIndex + 1);
+            return Math.Max(profitExclusive, profitInclusive);
+        }
+
+        [Theory]
+        [ClassData(typeof(GenerateData))]
+        public void Test(Knapsack knapsack)
+        {
+            var actual = SolveKnapsack(knapsack.Profits, knapsack.Weights, knapsack.Capacity);
+            Assert.Equal(knapsack.Result, actual);
+        }
+
+
+        public class Knapsack
+        {
+            public int[] Profits { get; set; }
+            public int[] Weights { get; set; }
+            public int Capacity { get; set; }
+            public int Result { get; set; }
+        }
+
+        public class GenerateData : IEnumerable<object[]>
+        {
+            public IEnumerator<object[]> GetEnumerator()
+            {
+                yield return new object[]
+                {
+                    new Knapsack()
+                    {
+                        Profits = new int[]{1, 6, 10, 16},
+                        Weights = new int[] {1, 2, 3, 5},
+                        Capacity = 7,
+                        Result = 22
+                    }
+                };
+                yield return new object[]
+                {
+                     new Knapsack()
+                    {
+                        Profits = new int[] { 1, 6, 10, 16 },
+                        Weights = new int[] { 1, 2, 3, 5 },
+                        Capacity = 6,
+                        Result = 17
+                    }
+                };
+
+            }
+
+            System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator()
+            {
+                return GetEnumerator();
+            }
+        }
+    }
+}
