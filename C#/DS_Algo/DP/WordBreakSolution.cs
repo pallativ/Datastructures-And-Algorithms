aee2f13073dac11d7fc05f24e6caf5de51c442d1
Veera Pallati <pkondalu@gmail.com>
2021-07-29 07:08:03 +0530

    Adding memoization logic

--- C#/DS_Algo/DP/WordBreakSolution.cs
+++ C#/DS_Algo/DP/WordBreakSolution.cs
@@ -5,42 +5,79 @@ using Xunit;
 
 namespace Datastructures.DP
 {
-    public class WordBreakBruteforce
+    public class WordBreakSolution
     {
-        public bool IsWordBreakPossible(string s, string[] wordDictionary)
+
+        public bool IsWordBreakBruteForce(string s, string[] wordDictionary)
         {
             // sub probelms are : word with single character, two character, three characters.
-            return IsWordBreakPossible(s, wordDictionary, 0);
+            return IsWordBreakBruteForce(s, wordDictionary, 0);
         }
-        public bool IsWordBreakPossible(string s, string[] wordDictionary, int start)
+        public bool IsWordBreakBruteForce(string s, string[] wordDictionary, int start)
         {
             if (start == s.Length)
                 return true;
-            for (int length = 1; length <= s.Length - start; length++)
+            for (int end = start + 1; end <= s.Length; end++)
             {
-                int j = start;
-                while (j < s.Length)
-                {
-                    var word = s.Substring(j, length);
-                    if (!Array.Exists(wordDictionary, t => t == word))
-                        break;
-                    j += word.Length;
-                    return IsWordBreakPossible(s, wordDictionary, j);
-                }
+                var word = s.Substring(start, end - start);
+                if (Array.Exists(wordDictionary, t => t == word)
+                    && IsWordBreakBruteForce(s, wordDictionary, end))
+                    return true;
             }
             return false;
+        }
+
+        bool?[] memo;
+        public bool IsWordBreakWithMemo(string s, string[] wordDictionary)
+        {
+            memo = new bool?[s.Length];
+            return IsWordBreakWithMemo(s, wordDictionary, 0);
+        }
+        public bool IsWordBreakWithMemo(string s, string[] wordDictionary, int start)
+        {
+            if (start == s.Length)
+                return true;
+            if (memo[start] != null)
+                return memo[start].Value;
+            for (int end = start + 1; end <= s.Length; end++)
+            {
+                var word = s.Substring(start, end - start);
+                if (Array.Exists(wordDictionary, t => t == word)
+                    && IsWordBreakWithMemo(s, wordDictionary, end))
+                    return (memo[start] = true).Value;
+            }
+            return (memo[start] = false).Value;
         }
 
         [Fact]
         public void Test()
         {
             bool result;
-            result = IsWordBreakPossible("leetcode", new string[] { "leet", "code" });
+            result = IsWordBreakBruteForce("leetcode", new string[] { "leet", "code" });
             Assert.True(result);
-            result = IsWordBreakPossible("applepenapple", new string[] { "apple", "pen" });
+            result = IsWordBreakBruteForce("applepenapple", new string[] { "apple", "pen" });
             Assert.True(result);
-            result = IsWordBreakPossible("catsandog", new string[] { "cats", "dog", "sand", "and", "cat" });
+            result = IsWordBreakBruteForce("catsandog", new string[] { "cats", "dog", "sand", "and", "cat" });
+            Assert.False(result);
+            result = IsWordBreakBruteForce("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaab", new string[] { "a", "aa", "aaa", "aaaa", "aaaaa", "aaaaaa", "aaaaaaa", "aaaaaaaa", "aaaaaaaaa", "aaaaaaaaaa" });
             Assert.False(result);
         }
+
+        [Fact]
+        public void TestWithMemo()
+        {
+            bool result;
+            //result = IsWordBreakWithMemo("leetcode", new string[] { "leet", "code" });
+            //Assert.True(result);
+            //result = IsWordBreakWithMemo("applepenapple", new string[] { "apple", "pen" });
+            //Assert.True(result);
+            //result = IsWordBreakWithMemo("catsandog", new string[] { "cats", "dog", "sand", "and", "cat" });
+            //Assert.False(result);
+            result = IsWordBreakWithMemo("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaab", new string[] { "a", "aa", "aaa", "aaaa", "aaaaa", "aaaaaa", "aaaaaaa", "aaaaaaaa", "aaaaaaaaa", "aaaaaaaaaa" });
+            Assert.False(result);
+
+        }
+
+
     }
 }
