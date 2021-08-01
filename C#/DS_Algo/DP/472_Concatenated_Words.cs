e6b01d1aa1898c6daa5cbc455599f598d5c78888
Veera Pallati <pkondalu@gmail.com>
2021-08-01 06:58:52 +0530

    Adding concatenated words

---
+++ C#/DS_Algo/DP/472_Concatenated_Words.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Datastructures.DP
+{
+    public class ConcatenatedWords
+    {
+        private Dictionary<int, List<List<string>>> memo = new Dictionary<int, List<List<string>>>();
+        private HashSet<string> wordsHashSet = new HashSet<string>();
+        public IList<string> FindAllConcatenatedWordsInADict(string[] words)
+        {
+            foreach (var word in words)
+            {
+                wordsHashSet.Add(word);
+            }
+            var result = new List<string>();
+            foreach (var sentance in words)
+            {
+                memo.Clear();
+                if (WordBreak(sentance))
+                    result.Add(sentance);
+            }
+            return result;
+        }
+
+        public bool WordBreak(string s)
+        {
+            WordBreakMemo(s, 0);
+            foreach (var item in memo[0])
+            {
+                if (item.Count >= 2)
+                    return true;
+            }
+            return false;
+        }
+        internal List<List<string>> WordBreakMemo(string s, int start)
+        {
+            if (start == s.Length)
+                return new List<List<string>>() { new List<string>() };
+
+            if (memo.ContainsKey(start))
+                return memo[start];
+            else
+                memo.Add(start, new List<List<string>>());
+
+            for (int length = start + 1; length <= s.Length; length++)
+            {
+                var currentWord = s[start..length];
+                if (wordsHashSet.Contains(currentWord))
+                {
+                    var postfixSentances = WordBreakMemo(s, length);
+                    foreach (var sentance in postfixSentances)
+                    {
+                        var newSentance = new List<string>(sentance) { currentWord };
+                        memo[start].Add(newSentance);
+                    }
+                }
+            }
+            return memo[start];
+        }
+
+        [Theory]
+        [ClassData(typeof(GenerateConcatenatedWordInput))]
+        public void Test(ConcatenatedWordInput input)
+        {
+            var actual = FindAllConcatenatedWordsInADict(input.WordsDictionary.ToArray());
+            Assert.Equal(input.Result, actual);
+        }
+    }
+
+    public class ConcatenatedWordInput
+    {
+        public List<string> WordsDictionary { get; set; }
+        public List<string> Result { get; set; }
+    }
+    public class GenerateConcatenatedWordInput : IEnumerable<object[]>
+    {
+        public IEnumerator<object[]> GetEnumerator()
+        {
+            yield return new object[]
+            {
+                    new ConcatenatedWordInput()
+                    {
+                       WordsDictionary = new List<string> (){ "cat","cats","catsdogcats","dog","dogcatsdog","hippopotamuses","rat","ratcatdogcat" },
+                       Result = new List<string> (){ "catsdogcats", "dogcatsdog", "ratcatdogcat" }
+                    }
+            };
+            yield return new object[]
+            {
+                    new ConcatenatedWordInput()
+                    {
+                       WordsDictionary = new List<string> (){"cat","dog","catdog"},
+                       Result = new List<string> (){ "catdog" }
+                    }
+            };
+
+        }
+
+        System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
