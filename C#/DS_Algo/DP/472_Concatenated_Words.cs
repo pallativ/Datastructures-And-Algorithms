f51b68ec4056a036d0f8efb3a719cc8daea7a0f9
Veera Pallati <pkondalu@gmail.com>
2021-08-01 07:25:16 +0530

    Fixing string empty case

--- C#/DS_Algo/DP/472_Concatenated_Words.cs
+++ C#/DS_Algo/DP/472_Concatenated_Words.cs
@@ -7,7 +7,6 @@ namespace Datastructures.DP
 {
     public class ConcatenatedWords
     {
-        private Dictionary<int, List<List<string>>> memo = new Dictionary<int, List<List<string>>>();
         private Dictionary<int, bool> boolMemo = new Dictionary<int, bool>();
         private HashSet<string> wordsHashSet = new HashSet<string>();
         public IList<string> FindAllConcatenatedWordsInADict(string[] words)
@@ -17,34 +16,14 @@ namespace Datastructures.DP
                 wordsHashSet.Add(word);
             }
             var result = new List<string>();
-            //foreach (var sentance in words)
-            //{
-            //    memo.Clear();
-            //    if (WordBreak(sentance))
-            //        result.Add(sentance);
-            //}
-            //return result;
-
             foreach (var sentance in words)
             {
                 boolMemo.Clear();
-                if (WordBreak(sentance, 0))
+                if (!string.IsNullOrEmpty(sentance) && WordBreak(sentance, 0))
                     result.Add(sentance);
             }
             return result;
         }
-
-        public bool WordBreak(string s)
-        {
-            WordBreakMemo(s, 0);
-            foreach (var item in memo[0])
-            {
-                if (item.Count >= 2)
-                    return true;
-            }
-            return false;
-        }
-
 
         internal bool WordBreak(string s, int start)
         {
@@ -57,6 +36,7 @@ namespace Datastructures.DP
             for (int length = start + 1; length <= s.Length; length++)
             {
                 var currentWord = s[start..length];
+                // currentWord != s - Important to skip the ones which are one word length.
                 if (currentWord != s && wordsHashSet.Contains(currentWord) && WordBreak(s, length))
                 {
                     boolMemo.Add(start, true);
@@ -65,31 +45,6 @@ namespace Datastructures.DP
             }
             boolMemo.Add(start, false);
             return boolMemo[start];
-        }
-        internal List<List<string>> WordBreakMemo(string s, int start)
-        {
-            if (start == s.Length)
-                return new List<List<string>>() { new List<string>() };
-
-            if (memo.ContainsKey(start))
-                return memo[start];
-            else
-                memo.Add(start, new List<List<string>>());
-
-            for (int length = start + 1; length <= s.Length; length++)
-            {
-                var currentWord = s[start..length];
-                if (wordsHashSet.Contains(currentWord))
-                {
-                    var postfixSentances = WordBreakMemo(s, length);
-                    foreach (var sentance in postfixSentances)
-                    {
-                        var newSentance = new List<string>(sentance) { currentWord };
-                        memo[start].Add(newSentance);
-                    }
-                }
-            }
-            return memo[start];
         }
 
         [Theory]
