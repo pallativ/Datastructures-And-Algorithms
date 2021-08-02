f28031f93258f637fea967a5e5c2d204c55dfb26
Veera Pallati <pkondalu@gmail.com>
2021-08-02 15:30:02 +0530

    Adding Best Buy and Best Sell

--- C#/DS_Algo/DP/BestTimeToBuyAndSellStock.cs
+++ C#/DS_Algo/DP/BestTimeToBuyAndSellStock.cs
@@ -10,8 +10,44 @@ namespace Datastructures.DP
         private Dictionary<int, int> memo = new Dictionary<int, int>();
         public int MaxProfit(int[] prices)
         {
-            return MaxProfitBruteForace(prices, 0);
+            return MaxProfitIII(prices, 0, 2);
+            //return MaxProfitBruteForace(prices, 0);
         }
+        public int OnePassSolution(int[] prices)
+        {
+            int maxProfit = 0;
+            for (int i = 1; i < prices.Length; i++)
+            {
+                if (prices[i] > prices[i - 1])
+                {
+                    maxProfit += prices[i] - prices[i - 1];
+                }
+            }
+            return maxProfit;
+        }
+
+        public int MaxProfitIII(int[] prices, int start, int remaining)
+        {
+            int minPrice = int.MaxValue;
+            int maxProfit = 0;
+            if (remaining == 0)
+                return 0;
+            //if (memo.ContainsKey(start))
+            //    return memo[start];
+            for (int i = start; i < prices.Length; i++)
+            {
+                if (prices[i] <= minPrice)
+                    minPrice = prices[i];
+                else
+                {
+                    var currentProfit = (prices[i] - minPrice) + MaxProfitIII(prices, i + 1, remaining - 1);
+                    maxProfit = Math.Max(maxProfit, currentProfit);
+                }
+            }
+            //memo.Add(start, maxProfit);
+            return maxProfit;
+        }
+
         public int MaxProfitBruteForace(int[] prices, int start)
         {
             int minPrice = int.MaxValue;
@@ -34,7 +70,8 @@ namespace Datastructures.DP
         [Fact]
         public void Test()
         {
-            var result = MaxProfit(new int[] { 7, 1, 5, 3, 6, 4 });
+            //var result = MaxProfit(new int[] { 7, 1, 5, 3, 6, 4 });
+            var result = MaxProfit(new int[] { 3, 3, 5, 0, 0, 3, 1, 4 });
         }
 
     }
---
+++ C#/DS_Algo/DP/BestTimeToBuyAndSellStockIII.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Datastructures.DP
+{
+    public class BestTimeToBuyAndSellStockIII
+    {
+        public int MaxProfit(int[] prices)
+        {
+            int?[][] memo = new int?[prices.Length][];
+            return MaxProfitIII(prices, 0, 2, memo);
+        }
+        public int MaxProfitIII(int[] prices, int start, int remaining, int?[][] memo)
+        {
+            int minPrice = int.MaxValue;
+            int maxProfit = 0;
+            if (remaining == 0)
+                return 0;
+            if (memo[start][remaining].HasValue)
+                return memo[start][remaining].Value;
+            for (int i = start; i < prices.Length; i++)
+            {
+                if (prices[i] <= minPrice)
+                    minPrice = prices[i];
+                else
+                {
+                    var currentProfit = (prices[i] - minPrice) + MaxProfitIII(prices, i + 1, remaining - 1, memo);
+                    maxProfit = Math.Max(maxProfit, currentProfit);
+                }
+            }
+            memo[start][remaining] = maxProfit;
+            return maxProfit;
+        }
+
+        [Fact]
+        public void Test()
+        {
+            //var result = MaxProfit(new int[] { 7, 1, 5, 3, 6, 4 });
+            var result = MaxProfit(new int[] { 3, 3, 5, 0, 0, 3, 1, 4 });
+        }
+
+    }
+}
