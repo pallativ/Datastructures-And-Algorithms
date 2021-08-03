c1980ab25dfa0867ce1602c7b5af9c53ce57dc2b
Veera Pallati <pkondalu@gmail.com>
2021-08-03 16:28:19 +0530

    241. Different Ways to Add Parentheses

--- C#/DS_Algo/241/Solution.cs
+++ C#/DS_Algo/241/Solution.cs
@@ -8,7 +8,48 @@ namespace Datastructures._241
     {
         public IList<int> DiffWaysToCompute(string expression)
         {
-
+            IList<int> ans = new List<int>();
+            for (int i = 0; i < expression.Length; i++)
+            {
+                if (IsOparator(expression[i]))
+                {
+                    var left = DiffWaysToCompute(expression[0..i]);
+                    var right = DiffWaysToCompute(expression[(i + 1)..expression.Length]);
+                    foreach (var num1 in left)
+                    {
+                        foreach (var num2 in right)
+                        {
+                            switch (expression[i])
+                            {
+                                case '+':
+                                    ans.Add(num1 + num2);
+                                    break;
+                                case '-':
+                                    ans.Add(num1 - num2);
+                                    break;
+                                case '*':
+                                    ans.Add(num1 * num2);
+                                    break;
+                            }
+                        }
+                    }
+                }
+            }
+            if (ans.Count == 0)
+                ans.Add(Convert.ToInt32(expression));
+            return ans;
+        }
+        private bool IsOparator(char op)
+        {
+            switch (op)
+            {
+                case '*':
+                case '+':
+                case '-':
+                    return true;
+                default:
+                    return false;
+            }
         }
     }
 }
---
+++ C#/Datastructures.Tests/241/UnitTest1.cs
@@ -0,0 +1,16 @@
+using Datastructures._241;
+using System;
+using Xunit;
+
+namespace Datastructures.Tests
+{
+    public class Tests
+    {
+        [Fact]
+        public void Test()
+        {
+            var solution = new Solution();
+            solution.DiffWaysToCompute("2-1-1");
+        }
+    }
+}
