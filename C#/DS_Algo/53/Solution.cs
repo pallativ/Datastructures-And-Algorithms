c464429ab7660495ecc4d1e33d7027680c6f7620
Veera Pallati <pkondalu@gmail.com>
2021-08-04 13:42:10 +0530

    Adding Max Sub array Sum

---
+++ C#/DS_Algo/53/Solution.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Datastructures._53
+{
+    public class Solution
+    {
+        private int?[] memo;
+        public int MaxSubArray(int[] nums)
+        {
+            memo = new int?[nums.Length + 1];
+            //return Naive(nums);
+            return DP(nums);
+        }
+        public int Naive(int[] nums)
+        {
+            int maxSum = int.MinValue;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                int currSum = 0;
+                for (int j = i; j < nums.Length; j++)
+                {
+                    currSum += nums[j];
+                    maxSum = Math.Max(currSum, maxSum);
+                }
+            }
+            return maxSum;
+        }
+        public int DP(int[] nums)
+        {
+            int currentSubArraySum = 0;
+            int maxSubAraySum = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                currentSubArraySum = Math.Max(nums[i], currentSubArraySum + nums[i]);
+                maxSubAraySum = Math.Max(maxSubAraySum, currentSubArraySum);
+            }
+            return maxSubAraySum;
+        }
+    }
+}
---
+++ C#/Datastructures.Tests/53/Tests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Datastructures.Tests._53
+{
+    public class Tests
+    {
+        [Fact]
+        public void Test()
+        {
+            var solution = new Datastructures._53.Solution();
+            var array = new int[] { -2, 1, -3, 4, -1, 2, 1, -5, 4 };
+            var result = solution.MaxSubArray(array);
+            Assert.Equal(6, result);
+        }
+    }
+}
